f75a2bb628b97b83d0ab7ac831c9136c3196af75
yxp99 <yxp99@case.edu>
2018-10-20 11:08:38 -0400

    Created the login test app

---
+++ Login/Login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by cj on 10/20/18.
+//  Copyright © 2018 3to1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
