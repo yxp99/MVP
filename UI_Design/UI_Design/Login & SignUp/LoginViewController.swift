a9e6e92bc741b3f0bfcb1a52bb9732b3dc112993
Shadow Song <shadowsong@Shadows-Mac-mini.local>
2018-12-26 14:22:01 -0500

    normal backup

---
+++ UI_Design/UI_Design/Login & SignUp/LoginViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LoginViewController.swift
+//  UI_Design
+//
+//  Created by Shadow Song on 12/26/18.
+//  Copyright © 2018 3to1. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func handleDismissButton(_ sender: Any) {
+        self.dismiss(animated: false, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
